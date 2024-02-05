18ae31344e4f790c028babfedc413fee9ca06545
ddbrn <ppq0qqp@gmail.com>
2024-02-05 23:12:45 +0100

    fix: remove font bold from card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ export default function Card(props: { url: string; title: string }) {
   const { url, title } = props;
   return (
     <div className="px-8 py-4 text-xl bg-transparent w-80 text-center border-2 border-black dark:border-white rounded-3xl hover:bg-white hover:text-black hover:cursor-pointer transition-all duration-100 ease-out">
-      <Link className="font-bold" href={url}>
+      <Link className="hover:font-bold" href={url}>
         {title}
       </Link>
     </div>
