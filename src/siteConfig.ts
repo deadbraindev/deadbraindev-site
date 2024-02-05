89447a0972f3ae485d54d50b1f33602ce6372699
ddbrn <ppq0qqp@gmail.com>
2024-02-05 20:42:40 +0100

    feat: adding site config file with metatags

---
+++ src/siteConfig.ts
@@ -0,0 +1,23 @@
+export const metaTags = {
+  title: 'deadbrain.dev',
+  description: '💀🧠',
+  publisher: 'deadbrain.dev',
+  category: 'portfolio',
+
+  // openGraph: {
+  //   title: 'strona główna | archiwum kulinarne',
+  //   siteName: 'archiwum kulinarne',
+  //   url: '/',
+  //   images: [
+  //     {
+  //       url: 'https://archiwumkulinarne.deadbrain.dev/images/opengraph-img-1200-630.jpg',
+  //       width: 1200,
+  //       height: 630,
+  //     },
+  //   ],
+  //   locale: 'pl_PL',
+  //   type: 'website',
+  // },
+};
+
+export default metaTags;
